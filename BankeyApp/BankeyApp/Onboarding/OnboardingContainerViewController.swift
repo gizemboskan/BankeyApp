4080dc0ade29ddf47501fe4be9ac22aac11b4189
Gizem Boskan <gizem.boskan.dev@gmail.com>
2022-03-24 15:29:21 +0300

    Complete onboarding scene

---
+++ BankeyApp/BankeyApp/Onboarding/OnboardingContainerViewController.swift
@@ -0,0 +1,122 @@
+//
+//  OnboardingContainerViewController.swift
+//  BankeyApp
+//
+//  Created by Gizem Boskan on 24.03.2022.
+//
+
+import UIKit
+
+final class OnboardingContainerViewController: UIViewController {
+    
+    let pageViewController: UIPageViewController
+    var pages = [UIViewController]()
+    var currentVC: UIViewController
+    let closeButton = UIButton(type: .system)
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        
+        let page1 = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989!")
+        let page2 = OnboardingViewController(heroImageName: "world", titleText: "Move your money around the world quickly and securely.")
+        let page3 = OnboardingViewController(heroImageName: "thumbs", titleText: "Learn more at www.bankey.com")
+        
+        pages.append(page1)
+        pages.append(page2)
+        pages.append(page3)
+        
+        currentVC = pages.first!
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        style()
+        layout()
+    }
+    
+    private func setup() {
+        view.backgroundColor = .systemPurple
+        
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.didMove(toParent: self)
+        
+        pageViewController.dataSource = self
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        pageViewController.setViewControllers([pages.first!], direction: .forward, animated: false, completion: nil)
+        currentVC = pages.first!
+    }
+    
+    private func style() {
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.setTitle("Close", for: [])
+        closeButton.addTarget(self, action: #selector(closeButtonPressed), for: .primaryActionTriggered)
+    }
+    
+    private func layout() {
+        view.addSubview(closeButton)
+        
+        // pageViewController
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: pageViewController.view.topAnchor),
+            view.leadingAnchor.constraint(equalTo: pageViewController.view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: pageViewController.view.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: pageViewController.view.bottomAnchor),
+        ])
+        
+        // Close
+        NSLayoutConstraint.activate([
+            closeButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            closeButton.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 2)
+        ])
+        
+    }
+    
+}
+
+// MARK: - UIPageViewControllerDataSource
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return getPreviousViewController(from: viewController)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return getNextViewController(from: viewController)
+    }
+    
+    private func getPreviousViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index - 1 >= 0 else { return nil }
+        currentVC = pages[index - 1]
+        return pages[index - 1]
+    }
+    
+    private func getNextViewController(from viewController: UIViewController) -> UIViewController? {
+        guard let index = pages.firstIndex(of: viewController), index + 1 < pages.count else { return nil }
+        currentVC = pages[index + 1]
+        return pages[index + 1]
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return pages.firstIndex(of: self.currentVC) ?? 0
+    }
+}
+
+// MARK: - Actions
+extension OnboardingContainerViewController {
+    @objc private func closeButtonPressed() {
+        
+    }
+}
