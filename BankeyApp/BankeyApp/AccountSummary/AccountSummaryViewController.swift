ba7f52d238b629749fca673bdc800bee5b744e86
Gizem Boskan <gizem.boskan.dev@gmail.com>
2022-03-27 19:57:37 +0300

    Add AccountSummaryHeaderView

--- BankeyApp/BankeyApp/AccountSummary/AccountSummaryViewController.swift
+++ BankeyApp/BankeyApp/AccountSummary/AccountSummaryViewController.swift
@@ -9,8 +9,69 @@ import UIKit
 
 final class AccountSummaryViewController: UIViewController {
     
+    let games = ["x", "y", "z"]
+    
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen
+        setup()
+    }
+}
+
+extension AccountSummaryViewController {
+    private func setup() {
+        setupTableView()
+        setupTableHeaderView()
+    }
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        style()
+        layout()
+    }
+    
+    private func style() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func setupTableHeaderView() {
+        let header = AccountSummaryHeaderView(frame: .zero)
+        
+        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        header.frame.size = size
+        
+        tableView.tableHeaderView = header
+    }
+}
+
+extension AccountSummaryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+}
+
+extension AccountSummaryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 }
---
+++ BankeyApp/BankeyApp/AccountSummary/Cells/AccountSummaryTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  AccountSummaryTableViewCell.swift
+//  BankeyApp
+//
+//  Created by Gizem Boskan on 27.03.2022.
+//
+
+import Foundation
+import UIKit
