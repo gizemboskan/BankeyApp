e569dfd42e2b4b5402110c4a847832c28b319133
Gizem Boskan <gizem.boskan.dev@gmail.com>
2022-03-28 18:37:11 +0300

    Refactor code

--- BankeyApp/BankeyApp/Utils/CurrencyFormatter.swift
+++ BankeyApp/BankeyApp/Utils/CurrencyFormatter.swift
@@ -11,7 +11,7 @@ struct CurrencyFormatter {
     
     func makeAttributedCurrency(_ amount: Decimal) -> NSMutableAttributedString {
         let tuple = breakIntoDollarsAndCents(amount)
-        return makeBalanceAttributed(dollars: tuple.0, cents: tuple.1)
+        return NSAttributedString().makeFormattedBalance(dollars: tuple.0, cents: tuple.1)
     }
     
     // Converts 929466.23 > "929,466" "23"
@@ -58,20 +58,5 @@ struct CurrencyFormatter {
         }
         
         return ""
-    }
-    
-    private func makeBalanceAttributed(dollars: String, cents: String) -> NSMutableAttributedString {
-        let dollarSignAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
-        let dollarAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .title1)]
-        let centAttributes: [NSAttributedString.Key: Any] = [.font: UIFont.preferredFont(forTextStyle: .callout), .baselineOffset: 8]
-        
-        let rootString = NSMutableAttributedString(string: "$", attributes: dollarSignAttributes)
-        let dollarString = NSAttributedString(string: dollars, attributes: dollarAttributes)
-        let centString = NSAttributedString(string: cents, attributes: centAttributes)
-        
-        rootString.append(dollarString)
-        rootString.append(centString)
-        
-        return rootString
     }
 }
